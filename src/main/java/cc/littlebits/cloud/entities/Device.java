77f11e0bc1999df04b2212ef85538892a175ef74
Paulo Pires <pjpires@gmail.com>
2016-03-28 17:54:40 -0400

    Added Javadoc for Device class.

--- src/main/java/cc/littlebits/cloud/entities/Device.java
+++ src/main/java/cc/littlebits/cloud/entities/Device.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 /**
- * Expected JSON output:
+ * This class represents a cloudBit device.
+ * <p>
+ * JSON example representation:
  * <pre>
  * {
  *   "label":"foobar",
@@ -21,25 +23,25 @@ import java.util.List;
  */
 public class Device {
 
-    private String label;
     private String id;
+    private String label;
+
     @JsonProperty("user_id")
     private long userId;
+
     @JsonProperty("is_connected")
     private boolean isConnected;
+
     @JsonProperty("input_interval_ms")
     private long inputIntervalMs;
+
     private List<Subscription> subscriptions;
+
     private List<Subscription> subscribers;
 
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
+    /**
+     * @return the cloudBit device identifier.
+     */
     public String getId() {
         return id;
     }
@@ -48,6 +50,20 @@ public class Device {
         this.id = id;
     }
 
+    /**
+     * @return the cloudBit device label.
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    /**
+     * @return the cloudBit owner user identifier.
+     */
     public long getUserId() {
         return userId;
     }
@@ -56,6 +72,9 @@ public class Device {
         this.userId = userId;
     }
 
+    /**
+     * @return the cloudBit current connection status.
+     */
     public boolean isConnected() {
         return isConnected;
     }
@@ -64,6 +83,10 @@ public class Device {
         isConnected = connected;
     }
 
+    /**
+     * @return the cloudBit current input interval (the interval at which the cloudBit reads and sends amplitude to the Cloud Platform).
+     * This value is in milliseconds.
+     */
     public long getInputIntervalMs() {
         return inputIntervalMs;
     }
@@ -72,6 +95,9 @@ public class Device {
         this.inputIntervalMs = inputIntervalMs;
     }
 
+    /**
+     * @return amplitude sources this cloudBit device is observing.
+     */
     public List<Subscription> getSubscriptions() {
         return subscriptions;
     }
@@ -80,6 +106,9 @@ public class Device {
         this.subscriptions = subscriptions;
     }
 
+    /**
+     * @return observers of this cloudBit device amplitude.
+     */
     public List<Subscription> getSubscribers() {
         return subscribers;
     }
